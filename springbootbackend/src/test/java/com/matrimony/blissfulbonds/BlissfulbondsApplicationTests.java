9d007c94a6c530115a4d3d0c637972527d4d73d9
sonali_gaikwad <sonalsgaikwad89@gmail.com>
2025-04-14 10:36:11 +0530

    springboot initial setup

---
+++ springbootbackend/src/test/java/com/matrimony/blissfulbonds/BlissfulbondsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matrimony.blissfulbonds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlissfulbondsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
